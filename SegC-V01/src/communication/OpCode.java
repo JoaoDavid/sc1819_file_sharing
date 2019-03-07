f66444f4f2e6f954d48da490ac518602e6091eff
M44TRIX <joaodavid512@gmail.com>
2019-03-07 21:40:19 +0000

    Update relatorio

--- SegC-V01/src/communication/OpCode.java
+++ SegC-V01/src/communication/OpCode.java
@@ -5,8 +5,10 @@ public enum OpCode {
 	STORE_FILES("store"), LIST_FILES("list"), REMOVE_FILES("remove"), 
 	USERS("users"), TRUST_USERS("trusted"), UNTRUST_USERS("untrusted"),
 	DOWNLOAD_FILE("download"), SEND_MSG("msg"), COLLECT_MSG("collect"), 
-	END_CONNECTION("exit"), OP_ERROR("error"), OP_SUCCESSFUL("OK"), 
-	OP_RES_ARRAY("op res in array"), OP_HELP("help"), OP_INVALID("invalid"),
+	END_CONNECTION("exit"),
+	
+	OP_ERROR("error"), OP_SUCCESSFUL("OK"), OP_RES_ARRAY("op res in array"),
+	
 	ERR_NOT_REGISTERED("not registered in the server"), ERR_ALREADY_EXISTS("already exists"),
 	ERR_NOT_FRIENDS("not friends"), ERR_YOURSELF("it's yourself"),
 	ERR_NOT_FOUND("not found");
