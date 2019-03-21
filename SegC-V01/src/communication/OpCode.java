b70c31ccb2da368a15301df7a2690a8f10f7cead
João Marques <fc49038@alunos.fc.ul.pt>
2019-03-21 22:13:11 +0000

    ESTAVEL FINAL

--- SegC-V01/src/communication/OpCode.java
+++ SegC-V01/src/communication/OpCode.java
@@ -11,7 +11,7 @@ public enum OpCode {
 	
 	ERR_NOT_REGISTERED("not registered in the server"), ERR_ALREADY_EXISTS("already exists"),
 	ERR_NOT_TRUSTED("not trusted"), ERR_YOURSELF("it's yourself"),
-	ERR_NOT_FOUND("not found"), STORE_FILES_I("store I");
+	ERR_NOT_FOUND("not found");
 	
 	private final String val;
 	/**
