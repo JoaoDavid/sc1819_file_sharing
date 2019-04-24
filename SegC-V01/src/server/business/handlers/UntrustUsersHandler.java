2202136ffd2274881cc347c32619f42bb2442c3c
M44TRIX <joaodavid512@gmail.com>
2019-04-24 19:23:23 +0100

    Finished UntrustUsersHandler

--- SegC-V01/src/server/business/handlers/UntrustUsersHandler.java
+++ SegC-V01/src/server/business/handlers/UntrustUsersHandler.java
@@ -1,18 +1,19 @@
 package server.business.handlers;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import facade.exceptions.ApplicationException;
+import security.ContentCipher;
 import server.business.util.FileManager;
 import server.business.util.FilePaths;
+import server.business.util.UserValidation;
 
 public class UntrustUsersHandler {
 
@@ -25,37 +26,46 @@ public class UntrustUsersHandler {
 	public boolean untrustUser(String userName, String userNameUntrusted, PrivateKey privKey, PublicKey pubKey) throws ApplicationException {
 		String filePath = FilePaths.FOLDER_SERVER_USERS + File.separator + userName 
 				+ File.separator + FilePaths.FILE_NAME_TRUSTED;
-		File untrustedFile = fileMan.acquireFile(filePath);
-		ArrayList<String> fileContent = new ArrayList<String>();
 		boolean untrusted = false;
-		try (BufferedReader br = new BufferedReader(new FileReader(untrustedFile));){
-			String st; 
-			while ((st = br.readLine()) != null) {
-
-				fileContent.add(st);
-
-			}
-			untrusted = fileContent.remove(userNameUntrusted);
-			if(untrusted) { //userNameUntrusted exists in the file
-				FileWriter fileWriter = new FileWriter(untrustedFile);
-				fileWriter.write("");
-				fileWriter.close();
-				fileWriter = new FileWriter(untrustedFile,true);
-				for(String str : fileContent) {
-					fileWriter.write(str + System.getProperty("line.separator"));
+		if(!UserValidation.isDeactivatedUser(userNameUntrusted) && UserValidation.userNameRegistered(userName) && !userName.equals(userNameUntrusted)) {
+			File trustedFile = fileMan.acquireFile(filePath);
+			try {
+				synchronized(trustedFile){
+					File trustedFileSig = new File(filePath + FilePaths.FILE_NAME_SIG_SUFIX);
+					File trustedFileKey = new File(filePath + FilePaths.FILE_NAME_KEY_SUFIX);
+					byte[] result = ContentCipher.decryptFileAndCheckSig(trustedFile, trustedFileSig, trustedFileKey, privKey, pubKey);
+					String inFile = new String(result);
+					List<String> list = Arrays.asList(inFile.split("\n"));
+					ByteArrayOutputStream byteArr = new ByteArrayOutputStream();
+					for(String currLine : list) {
+						System.out.println("CURR TRUSTED:"+currLine+"|");
+						if(currLine.equals(userNameUntrusted)) {
+							untrusted = true;
+						}else {
+							byteArr.write((currLine + "\n").getBytes());
+						}
+					}
+					if(untrusted) { //userNameUntrusted was deleted from the file
+						ContentCipher.sigAndEcryptFile(trustedFile, trustedFileSig, trustedFileKey, privKey, pubKey, byteArr.toByteArray());
+					}//else, does not exist in the file, so there is no need to rewrite the file
+					return untrusted;
 				}
-				fileWriter.close();
-			}//else, does not exist in the file, so there is no need to rewrite the file
-			return untrusted;
-		} catch (FileNotFoundException e) {
-			throw new ApplicationException("File not found at: " + filePath);
-		} catch (IOException e) {
-			throw new ApplicationException("IOException in untrustUser");
-		} finally {
-			fileMan.releaseFile(filePath);
+			} catch (FileNotFoundException e) {
+				throw new ApplicationException("File not found at: " + filePath);
+			} catch (IOException e) {
+				throw new ApplicationException("IOException in untrustUser");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				fileMan.releaseFile(filePath);
+			}
 		}
-
-
+		return untrusted;
 	}
-
+	
+	
 }
+
+
+
