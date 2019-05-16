d8632e84ba8e6d5b8cdd6f4f067a3bbe06140330
Luís Miguel Pereira Moreira <fc49531@alunos.fc.ul.pt>
2019-05-16 11:08:37 +0100

    Replace iptable-config.sh

--- SegC-V01/iptable-config.sh
+++ SegC-V01/iptable-config.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+LOCAL_IP="10.101.149.51"
+
+#clean Chains
+sudo /sbin/iptables -F
+
 #loopback nao filtrado
 sudo /sbin/iptables -A INPUT -i lo -j ACCEPT
 sudo /sbin/iptables -A OUTPUT -o lo -j ACCEPT
@@ -9,52 +14,50 @@ sudo /sbin/iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 sudo /sbin/iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 #Ping - para as maquinas locais max.2 por segundo
-sudo iptables -A OUTPUT -p icmp -m icmp --icmp-type 8 -s 10.101.148.0/23 -m limit --limit 2/second -j ACCEPT
+sudo /sbin/iptables -A INPUT -p icmp -m icmp --icmp-type 8 -s 10.101.148.0/23 -d 10.101.148.0/23 -m limit --limit 2/s --limit-burst 1 -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -p icmp -m icmp --icmp-type 8 -d 10.101.148.0/23 -s 10.101.148.0/23 -m limit --limit 2/s --limit-burst 1 -j ACCEPT
 
 #DCs
-sudo /sbin/iptables -A INPUT -s 10.121.52.14 -p tcp -j ACCEPT
-sudo /sbin/iptables -A OUTPUT -d 10.121.52.14 -p tcp -j ACCEPT
+sudo /sbin/iptables -A INPUT -s 10.121.52.14 -d $LOCAL_IP -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -d 10.121.52.14 -s $LOCAL_IP -j ACCEPT
 
-sudo /sbin/iptables -A INPUT -s 10.121.52.15 -p tcp -j ACCEPT
-sudo /sbin/iptables -A OUTPUT -d 10.121.52.15 -p tcp -j ACCEPT
+sudo /sbin/iptables -A INPUT -s 10.121.52.15 -d $LOCAL_IP -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -d 10.121.52.15 -s $LOCAL_IP -j ACCEPT
 
-sudo /sbin/iptables -A INPUT -s 10.121.52.16 -p tcp -j ACCEPT
-sudo /sbin/iptables -A OUTPUT -d 10.121.52.16 -p tcp -j ACCEPT
+sudo /sbin/iptables -A INPUT -s 10.121.52.16 -d $LOCAL_IP -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -d 10.121.52.16 -s $LOCAL_IP -j ACCEPT
 
 #Storage
-sudo /sbin/iptables -A INPUT -s 10.121.72.23 -p tcp -j ACCEPT
-sudo /sbin/iptables -A OUTPUT -d 10.121.72.23 -p tcp -j ACCEPT
+sudo /sbin/iptables -A INPUT -s 10.121.72.23 -d $LOCAL_IP -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -d 10.121.72.23 -s $LOCAL_IP -j ACCEPT
 
 #Iate/Falua
-sudo /sbin/iptables -A INPUT -s 10.101.85.6 -p tcp -j ACCEPT
-sudo /sbin/iptables -A OUTPUT -d 10.101.85.6 -p tcp -j ACCEPT
+sudo /sbin/iptables -A INPUT -s 10.101.85.6 -d $LOCAL_IP -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -d 10.101.85.6 -s $LOCAL_IP -j ACCEPT
 
-sudo /sbin/iptables -A INPUT -s 10.101.85.138 -p tcp -j ACCEPT
-sudo /sbin/iptables -A OUTPUT -d 10.101.85.138 -p tcp -j ACCEPT
+sudo /sbin/iptables -A INPUT -s 10.101.85.138 -d $LOCAL_IP -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -d 10.101.85.138 -s $LOCAL_IP -j ACCEPT
 
 #Luna
-sudo /sbin/iptables -A INPUT -s 10.101.85.24 -p tcp -j ACCEPT
-sudo /sbin/iptables -A OUTPUT -d 10.101.85.24 -p tcp -j ACCEPT
+sudo /sbin/iptables -A INPUT -s 10.101.85.24 -d $LOCAL_IP -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -d 10.101.85.24 -s $LOCAL_IP -j ACCEPT
 
 #Gateway
-sudo /sbin/iptables -A INPUT -s 10.101.148.1 -p tcp -j ACCEPT
-sudo /sbin/iptables -A OUTPUT -d 10.101.148.1 -p tcp -j ACCEPT
+sudo /sbin/iptables -A INPUT -s 10.101.148.1 -d $LOCAL_IP -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -d 10.101.148.1 -s $LOCAL_IP -j ACCEPT
 
 #Proxy
-sudo /sbin/iptables -A INPUT -s 10.101.85.134 -p tcp -j ACCEPT
-sudo /sbin/iptables -A OUTPUT -d 10.101.85.134 -p tcp -j ACCEPT
+sudo /sbin/iptables -A INPUT -s 10.101.85.134 -d $LOCAL_IP -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -d 10.101.85.134 -s $LOCAL_IP -j ACCEPT
 
 #Ping - gcc
-sudo /sbin/iptables -A INPUT -s 10.101.151.5 -p icmp -j ACCEPT
-sudo /sbin/iptables -A OUTPUT -d 10.101.151.5 -p icmp -j ACCEPT
-
-#sudo /sbin/iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -m iprange -s 10.101.148.0/23 -j ACCEPT
+sudo /sbin/iptables -A INPUT -s 10.101.151.5 -p icmp -d $LOCAL_IP -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -d 10.101.151.5 -p icmp -s $LOCAL_IP -j ACCEPT
 
 #Mask 255.255.254.0
 
-#SSH - maquinas Sala 1.3.12
-sudo /sbin/iptables -A INPUT -s 10.101.148.0/23 -p tcp -j ACCEPT
-sudo /sbin/iptables -A OUTPUT -d 10.101.148.0/23 -p tcp -j ACCEPT
+#SSH - maquinas
+sudo /sbin/iptables -A INPUT -s 10.101.148.0/23 -p tcp -d $LOCAL_IP --dport 22 -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -d 10.101.148.0/23 -p tcp -s $LOCAL_IP --dport 22 -j ACCEPT
 
 #sudo /sbin/iptables -P OUTPUT DROP
-#sudo /sbin/iptables -F OUTPUT
